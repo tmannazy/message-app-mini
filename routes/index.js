bd6fe23b81c18461879962c54e360b68b5bb5b22
tmannazy <e.anene@hotmail.com>
2022-11-01 20:12:31 +0100

    Create a new view to add new messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var jsonParser = router.use(express.json());
+var urlencoded = router.use(express.urlencoded({ extended: true }));
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Tman",
+    added:
+      new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString(),
+  },
+  {
+    text: "Hello Programmer!",
+    user: "Nazy",
+    added:
+      new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  // res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("form", {});
+});
+
+router.post("/new", (req, res, next) => {
+  let newMessages = JSON.parse(JSON.stringify(req.body));
+  // console.log(req.body.message);
+  messages.push({
+    text: newMessages.message,
+    user: newMessages.author,
+    added:
+      new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
